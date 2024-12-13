bf551e714527b5bc686f01e61e1cf45825158ddd
Jaco <jmaster85@hotmail.com>
2024-12-13 17:08:07 +0100

    Update main.js

--- main.js
+++ main.js
@@ -8,10 +8,11 @@ const parser2 = new ContentParser({
 
 // Multiple divisions and custom JSON URL
 const parser3 = new ContentParser({
-    jsonURL: 'https://custom-disclaimers.com/file.json',
-    currentDivision: 'MX, AV',
+    jsonURL: 'https://raw.githubusercontent.com/aith85/ss/refs/heads/main/test.json',
+    currentDivision: 'ALL',
     allowedDomains: [
         'www.samsung.com',
-        'custom-samsung-domain.com'
+        'p6-qa.samsung.com',
+        'p6-eu-author.samsung.com/content/samsung',
     ]
 });
