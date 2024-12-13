3205e97f6ebdb82884d5425774d785e71def6c6e
Jaco <jmaster85@hotmail.com>
2024-12-13 17:17:00 +0100

    Update main.js

--- main.js
+++ main.js
@@ -1,18 +1,17 @@
-// All default settings
-const parser1 = new ContentParser();
-
-// Specific division
-const parser2 = new ContentParser({
-    currentDivision: 'MX'
+const parser = new ContentParser({
+    currentDivision: 'MX',
+    disclaimerContainerId: 'cheilDisclaimers'
 });
+parser.insertPageContents();
 
-// Multiple divisions and custom JSON URL
-const parser3 = new ContentParser({
+const parser2 = new ContentParser({
     jsonURL: 'https://raw.githubusercontent.com/aith85/ss/refs/heads/main/test.json',
     currentDivision: 'ALL',
     allowedDomains: [
         'www.samsung.com',
         'p6-qa.samsung.com',
         'p6-eu-author.samsung.com/content/samsung',
-    ]
+    ],
+    disclaimerContainerId: 'cheilDisclaimers'
 });
+parser2.insertPageContents();
